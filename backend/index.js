927898ebe63854c6542de20ab63f79ace83382db
Pavi99 <pavithra@smbapps.com.au>
2023-01-08 15:22:06 +0530

    backend created

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var express = require('express');
+var port = process.env.PORT || 8080;
+var app = express();
+var appRoute = require('./routes/appRoutes');
+var mongoose = require('mongoose');
+
+var res=mongoose.connect('mongodb://localhost/meanDb', { useNewUrlParser: true });
+if(res){
+    console.log("connected");
+}else{
+    console.log("not");
+}
+app.use('/', appRoute);
+
+http.createServer(app).listen(port);
+
+console.log("server is running on port:", port);
